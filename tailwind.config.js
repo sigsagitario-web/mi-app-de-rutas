b1c05c8167459f497030b824bebaa6430a158de8
sigsagitario-web <sigsagitario@gmail.com>
2025-09-01 19:19:12 -0500

    feat: especifica la version de node y npm para vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
- content: [
-  "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx,html}",
-  "./public/**/*.html",
-],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}", // si tus componentes están aquí
+    "./pages/**/*.{js,ts,jsx,tsx}",      // si tus páginas están aquí
+  ],
   theme: {
     extend: {},
   },
